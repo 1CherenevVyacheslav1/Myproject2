12d9cce414531c79640aed3690e164eff8066066
1CherenevVyacheslav1 <76144560+1CherenevVyacheslav1@users.noreply.github.com>
2020-12-19 08:26:12 +0900

    Update MyProject2.java

--- MyProject2/src/myproject2/MyProject2.java
+++ MyProject2/src/myproject2/MyProject2.java
@@ -1,96 +1,140 @@
 
 package myproject2;
 
-import javafx.application.Application;import java.awt.BorderLayout;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 import java.io.*;
-
-public class Myproject2 extends JFrame 
-{
-    private int balans = 0;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+        
+public class Myproject2 extends JFrame {
+ int balans = 0;
+ int bal = 0;
+  int i =0;
+  int f = 0;
+  
     private JLabel countLabel;
     private JLabel prognoz;
     private JButton calc;
     private JTextField text1;
     private JTextField text2;
+    
 
-    public Myproject2() 
-    {
-       super("счетчик финансов"); // конструктор базового класса
-       /* Подготавливаем компоненты объекта */
-       countLabel = new JLabel("бюджет: " + balans + "     ");
-       prognoz = new JLabel("прогноз на следующий месяц: " + balans);
-       calc = new JButton("посчитать");
-       text1 = new JTextField("введите доход",20);
-       text2 = new JTextField("введите расходы",20);
-       /* Подготавливаем временные компоненты */
-       JPanel buttonsPanel = new JPanel(new FlowLayout());
-       JPanel TextField = new JPanel(new FlowLayout());
+public Frame() throws IOException {
+super("счетчик финансов"); // конструктор базового класса
+/* Подготавливаем компоненты объекта  */
+ countLabel = new JLabel("бюджет: " + balans + "     ");
+ prognoz = new JLabel("прогноз на следующий месяц: " + balans);
+ calc = new JButton("посчитать");
+ text1 = new JTextField("введите доход",20);
+ text2 = new JTextField("введите расходы",20);    
+/* Подготавливаем временные компоненты  */
+  JPanel buttonsPanel = new JPanel(new FlowLayout());
+        JPanel TextField = new JPanel(new FlowLayout());
        JPanel label = new JPanel(new FlowLayout());
        /* Расставляем компоненты по местам */
-       label.add(countLabel);
-       label.add(prognoz);
-       add(label, BorderLayout.NORTH);
-       buttonsPanel.add(calc);
-       add(buttonsPanel, BorderLayout.SOUTH);
+ label.add(countLabel);
+ label.add(prognoz);
+ add(label, BorderLayout.NORTH);
+ buttonsPanel.add(calc);
+ add(buttonsPanel, BorderLayout.SOUTH);
        TextField.add(text1);
        TextField.add(text2);
-       add(TextField, BorderLayout.CENTER);
-       initListeners();
+ add(TextField, BorderLayout.CENTER);
+    
+ initListeners();
     }
 
- private void initListeners()
- {
-  // Добавляем listener для кнопки acalc
-  // У объекта нет имени, поскольку используется только один раз.
-  calc.addActionListener(new ActionListener()
-  // У класса также нет имени - анонимный внутренний класс.
-   {
-       public void actionPerformed(ActionEvent e) {
-           int x = 0, y = 0;
-           try 
-           {
-               y = Integer.parseInt(text2.getText());
-               x = Integer.parseInt(text1.getText());
-           }
-           catch (NumberFormatException e1) 
-           {
-               JOptionPane.showMessageDialog(countLabel, "Некорректный ввод!");
-           }
-           balans = x - y ;
-           updatechetchik();
-       }
-   }
-  );
- }
-//Обновляем бюджет
- private void updatechetchik() 
- {
-     countLabel.setText("бюджет: " + balans+"     ");
-     prognoz.setText("прогноз на следующий месяц:  " + balans * 1.01 );
- }
- public static void main(String[] args) throws IOException 
- {
-     Myproject2 app = new Myproject2();
-     app.setVisible(true);
-     app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     app.pack(); /* оптим. размер в завис. от содержимого окна */
-    /* File file = new File("ballanc.txt");
-      if(!file.exists())
-     {
-         file.createNewFile();
-        System.out.println("ФАЙЛ СОЗДАН");
-         FileWriter filewr = new FileWriter(file);
-         filewr.write(x);
-     }
-     else
-     {
-        System.out.println("ФАЙЛ существует");
-
-     }*/
- }
-}
+public void initListeners() {
+// Добавляем listener для кнопки addCrow
+// У объекта нет имени, поскольку используется только один раз.
+      calc.addActionListener(new ActionListener()
+// У класса также нет имени - анонимный внутренний класс.
+{
+@SuppressWarnings("empty-statement")
+public void actionPerformed(ActionEvent e) {
    
+    FileWriter fw = null;
+    
+        try {
+            FileWriter filewr = null;
+            
+        int x = 0, y = 0;
+        try {
+            y = Integer.parseInt(text2.getText());
+            x = Integer.parseInt(text1.getText());
+        } catch (NumberFormatException e1) {
+            JOptionPane.showMessageDialog(countLabel, "Некорректный ввод!");
+        }
+        balans = x - y ;
+        // Возьмите файл
+        File f = new File("ballans1.txt");
+        fw = new FileWriter(f,true);
+       FileReader fr = new FileReader(f);
+        try {
+            //Создайте новый файл
+            // Убедитесь, что он не существует
+            if (f.createNewFile())
+                System.out.println("File created");
+            else
+            {
+                System.out.println("File already exists");
+                String str1 = Integer.toString(balans);
+                fw.write(str1);
+                fw.write("\n");
+                fw.flush();
+                fw.close();
+
+                 BufferedReader bufr = new BufferedReader(fr);
+                while(bufr.ready()){
+                        int bal = Integer.parseInt(bufr.readLine());
+                        i=i+bal;
+                        updatechetchik(i,bal);
+ 
+            }
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        } catch (IOException ex) {
+            Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null,ex);
+        } finally {
+        try {
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        }
+       
+       
+    
+              
+}
+);
+}
+
+//Обновляем chetcik
+private void updatechetchik(int i,int bal) 
+ {
+
+       System.out.println(bal);
+ countLabel.setText("бюджет: " + balans +"     ");
+ prognoz.setText("прогноз на следующий месяц: " + balans * 1.01 );
+ }
+public static void main(String[] args) throws IOException {
+Frame app = new Frame();
+app.setVisible(true);
+app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+app.pack(); 
+
+
+}
+
+} 
